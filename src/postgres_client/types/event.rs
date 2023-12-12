7cccc54819fc10ed25919904cbea623d0f9341e3
Todd Martin <kj4ntv@gmail.com>
2023-12-11 20:34:15 -0800

    add event type

---
+++ src/postgres_client/types/event.rs
@@ -0,0 +1,22 @@
+use postgres_types::FromSql;
+
+use super::replication_state::ReplicationState;
+
+#[derive(Debug, FromSql)]
+pub struct Event {
+    eventid: i64, // bigint
+    eventtime: std::time::SystemTime, // timestamp w/timezone
+    formationid: String, // text
+    nodeid: i64, // bigint
+    groupip: i32, // integer
+    nodename: String, // text
+    nodehost: Strong, // text
+    nodeport: i32, // integer
+    reportedstate: ReplicationState, // replication_state
+    goalstate: ReplicationState, // replication_state
+    reportedtli: i32, // integer
+    reportedlsn: postgres_types::PgLsn, // pglsn
+    candidatepriority: i32, // integer
+    replicationquorum: bool, // boolean
+    description: String, // text
+}
