56f1e7b76fbf0c1ed2999983d23267b9894e1d7f
Todd Martin <kj4ntv@gmail.com>
2023-12-11 18:57:33 -0800

    Remove optionals and default

--- src/postgres_client/types/node.rs
+++ src/postgres_client/types/node.rs
@@ -2,27 +2,27 @@ use postgres_types::{ FromSql };
 
 use super::replication_state::ReplicationState;
 
-#[derive(Debug, Default, FromSql)]
+#[derive(Debug, FromSql)]
 pub struct Node {
-    pub formationid: Option<String>, //text
-    pub nodeid: Option<i64>, // bigint
-    pub groupid: Option<i32>, // integer
-    pub nodename: Option<String>, // text
-    pub nodehost: Option<String>, // text
-    pub nodeport: Option<i32>, // integer
-    pub systemidentifier: Option<i64>, // bigint
-    pub goalstate: Option<ReplicationState>, // replication_state
-    pub reportedstate: Option<ReplicationState>, // replication_state
+    pub formationid: String, //text
+    pub nodeid: i64, // bigint
+    pub groupid: i32, // integer
+    pub nodename: String, // text
+    pub nodehost: String, // text
+    pub nodeport: i32, // integer
+    pub systemidentifier: i64, // bigint
+    pub goalstate: ReplicationState, // replication_state
+    pub reportedstate: ReplicationState, // replication_state
     pub reportedpgisrunning: bool, // bool
-    pub reportedrepstate: Option<String>, // text
-    pub reporttime: Option<std::time::SystemTime>, // time stamp w/ timezone
-    pub reportedtli: Option<i32>, // integer
-    pub reportedlsn: Option<postgres_types::PgLsn>, // pg_lsn
-    pub walreporttime: Option<std::time::SystemTime>, // timestamp w/ timezone
-    pub health: Option<i32>, // integer
-    pub healthchecktime: Option<std::time::SystemTime>, // timestamp w/timezone
-    pub statechangetime: Option<std::time::SystemTime>, // timestampe w/ timezone
-    pub candidatepriority: Option<i32>, // integer
+    pub reportedrepstate: String, // text
+    pub reporttime: std::time::SystemTime, // time stamp w/ timezone
+    pub reportedtli: i32, // integer
+    pub reportedlsn: postgres_types::PgLsn, // pg_lsn
+    pub walreporttime: std::time::SystemTime, // timestamp w/ timezone
+    pub health: i32, // integer
+    pub healthchecktime: std::time::SystemTime, // timestamp w/timezone
+    pub statechangetime: std::time::SystemTime, // timestampe w/ timezone
+    pub candidatepriority: i32, // integer
     pub replicationquorum: bool, // boolean
-    pub nodecluster: Option<String>, // text
+    pub nodecluster: String, // text
 }
